ae2195650d4850e08d6c968bd17ab3cbe6569b65
Leo Garcez <garceleo@gmail.com>
2025-07-17 18:09:13 -0300

    fix: config plugin

--- app.plugin.js
+++ app.plugin.js
@@ -1,1 +1,1 @@
-module.exports = require("./build/withBanuba");
+module.exports = require("./plugin/build/withBanuba");
--- src/ExpoBanubaModule.ts
+++ src/ExpoBanubaModule.ts
@@ -1,5 +1,17 @@
-import { requireNativeModule } from "expo-modules-core";
+import { NativeModule, requireNativeModule } from "expo-modules-core";
+import { ConfigPlugin } from "@expo/config-plugins";
 
-// It loads the native module object from the JSI or falls back to
-// the bridge module (from NativeModulesProxy) if the remote debugger is on.
-export default requireNativeModule("ExpoBanuba");
+export type BanubaPluginProps = {
+	android: {
+		assetsPath: string;
+	};
+	ios: {
+		assetsPath: string;
+	};
+};
+
+declare class ExpoNativeConfigurationModule extends NativeModule {
+	withBanuba(): ConfigPlugin<BanubaPluginProps>;
+}
+
+export default requireNativeModule<ExpoNativeConfigurationModule>("ExpoBanuba");
